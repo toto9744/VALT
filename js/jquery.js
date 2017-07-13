8e1c6e99bd35b9270003675e8a6361947579402b
LauGR <laurent.gregoire974@gmail.com>
2017-07-13 19:37:58 +0400

    pti pas it pas

--- js/jquery.js
+++ js/jquery.js
@@ -1,25 +1,6 @@
-$('.center').slick({
-  centerMode: true,
-  centerPadding: '60px',
+$('.autoplay').slick({
   slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
 });
