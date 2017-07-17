728b0f2d0bbb365ee5679b01970a9593895e81b7
Alexandre Callens <alekzxand@gmail.com>
2017-07-17 14:24:27 +0400

    ajout images

--- js/jquery.js
+++ js/jquery.js
@@ -7,4 +7,31 @@ $(document).ready(function(){
 	$('.carousel-caption').hide()
 	$('.carousel-caption').show(3300)
 
+$('.slider').on(function(){
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+});
 });
