3e3ed221bef623d9143b585ca912b2dd65f480fe
Alexandre Callens <alekzxand@gmail.com>
2017-07-18 14:24:39 +0400

    animation

--- js/jquery.js
+++ js/jquery.js
@@ -2,14 +2,24 @@ $(document).ready(function(){
 	$('.formulaire').hide();
 	$('.contact').click(function(){
 	$('.formulaire').toggle('slow')	
+});
+	
 
-	});
     $('li:nth-child(1)').mouseenter(function(){
-    $('.trait_anime').animate({width: '+=140px' },1000)});
+    $('.trait_anime').animate({width: '+=160px' },1000)});
+    $('li:nth-child(1)').mouseout(function(){
+    $('.trait_anime').animate({width: '-=160px' },1000)});
+
     $('li:nth-child(2)').mouseenter(function(){
-    $('.trait_anime2').animate({width: '+=120px' },1000)});
+    $('.trait_anime2').animate({width: '+=140px' },1000)});
+      $('li:nth-child(2)').mouseout(function(){
+    $('.trait_anime2').animate({width: '-=140px' },1000)});
+
     $('li:nth-child(3)').mouseenter(function(){
     $('.trait_anime3').animate({width: '+=120px' },1000)});
+       $('li:nth-child(3)').mouseout(function(){
+    $('.trait_anime3').animate({width: '-=120px' },1000)});
+
   })
 
 
